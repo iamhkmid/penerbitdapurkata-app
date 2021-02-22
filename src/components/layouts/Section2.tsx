78c7022732d6e6e13fd99a225f3c0c2abd824c6e
iamhkmid <iamhkmid@gmail.com>
2021-02-23 04:15:52 +0700

    git init

--- src/components/layouts/Section2.tsx
+++ src/components/layouts/Section2.tsx
@@ -33,9 +33,9 @@ const Section2 = forwardRef(() => {
     ]
 
     return (
-        <div ref={sectionRef} className=" py-5 bg-gray-50 font-roboto bg-gradient-to-r from-gray-100 to-white">
+        <div ref={sectionRef} className=" py-14 bg-gray-50 font-roboto bg-gradient-to-r from-gray-100 to-white">
             <h1 className="text-3xl font-bold uppercase text-center pt-3">Spesifikasi Naskah</h1>
-            <hr className="text-gray-500 border-t-2 border-gray-300 mx-24 pb-5"></hr>
+            <hr className="text-gray-500 border-t-2 border-gray-300 mx-24 pb-8"></hr>
             <div className="flex flex-wrap justify-items-center items-center place-content-evenly">
                 {naskahSpecs.map((data, index) => (
                     <div key={data.name} className="group bg-gray-100 text-gray-900 hover:bg-gray-200 border-transparent border-4 hover:border-indigo-200 rounded-md shadow-md cursor-default my-5">
@@ -47,6 +47,7 @@ const Section2 = forwardRef(() => {
                                 </svg>
                                 <div className="flex-grow px-5 py-2">
                                     <h1 className="text-2xl font-bold uppercase "> {data.name}</h1>
+                                    <hr className="text-gray-500 border-t-2 border-gray-300 pb-3"></hr>
                                     <div>
 
                                         <h1 className="text-base font-medium "><span className="bg-indigo-200 rounded-lg px-1">Warna</span> : {data.warna}</h1>
@@ -62,6 +63,7 @@ const Section2 = forwardRef(() => {
                                     <svg className="flex-grow-0 text-gray-600  h-10 pt-2 pl-2" xmlns='http://www.w3.org/2000/svg' fill="currentColor" viewBox='0 0 512 512'><title>Book</title><path d='M256 160c16-63.16 76.43-95.41 208-96a15.94 15.94 0 0116 16v288a16 16 0 01-16 16c-128 0-177.45 25.81-208 64-30.37-38-80-64-208-64-9.88 0-16-8.05-16-17.93V80a15.94 15.94 0 0116-16c131.57.59 192 32.84 208 96zM256 160v288' fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='32' /></svg>
                                     <div className="flex-grow px-5 py-2">
                                         <h1 className="text-2xl font-bold uppercase "> {data.name}</h1>
+                                        <hr className="text-gray-500 border-t-2 border-gray-300 pb-3"></hr>
                                         <div>
                                             <div className="text-base font-medium "><span className="bg-indigo-200 rounded-lg px-1">Warna</span> : {data.warna}</div>
                                             <div className="text-base font-medium "><span className="bg-indigo-200 rounded-lg px-1">Jenis Kertas</span>: {data.jenisKertas}</div>
@@ -77,6 +79,7 @@ const Section2 = forwardRef(() => {
                                         </svg>
                                         <div className="flex-grow px-5 py-2">
                                             <h1 className="text-2xl font-bold uppercase "> {data.name}</h1>
+                                            <hr className="text-gray-500 border-t-2 border-gray-300 pb-3"></hr>
                                             {data.values.map((item, index) => (
                                                 <h1 key={item} className="text-base font-medium ">{item}</h1>
                                             ))}
