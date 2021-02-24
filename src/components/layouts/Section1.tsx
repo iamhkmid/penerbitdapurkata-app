ed4711d066c91cd15f4b00dec9c9feacd5c9ebf6
iamhkmid <iamhkmid@gmail.com>
2021-02-24 20:08:50 +0700

    git initial

--- src/components/layouts/Section1.tsx
+++ src/components/layouts/Section1.tsx
@@ -32,7 +32,7 @@ const Section1 = forwardRef(() => {
                         </h1>
 
                             <h1 className="font-extrabold text-2xl md:text-4xl mt-5 md:mt-10 text-gray-900" >
-                                "Di sini naskahmu diracik dengan sempurna."
+                                "Di sini, naskahmu diracik dengan sempurna."
                         </h1>
                             <h1 className="font-normal text-base md:text-xl lg:text-xl  mt-5 md:mt-10 text-justify text-gray-900">
                                 "Dalam banyak diskusi tentang perubahan zaman, buku selalu mendapat tempat khusus sebagai energi penggerak manusia untuk menciptakan sesuatu. Dari sini, kita paham bahwa buku selalu hadir sebagai bagian penting dalam peradaban.Bersama buku, kita menyempurnakan kemanusiaan.Bersama buku, kita mendorong perubahan.Tindakan ini dimulai dari kita, dimulai dari Anda.Mari berkolaborasi menciptakan buku - buku terbaik! Buku Anda akan menjadi bagian dari sejarah."
