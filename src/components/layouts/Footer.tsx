9df488f70af26aa55af4f197c7bc9e072826fd72
iamhkmid <iamhkmid@gmail.com>
2021-03-03 21:30:52 +0700

    revisi

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -4,16 +4,16 @@ import SosialMedia from "../SosialMedia";
 const Footer = () => {
   return (
     <div className="flex flex-col bg-gray-900">
-      <div className="flex flex-wrap justify-center py-10 bg-gray-800 ">
-        <div className="w-auto md:w-1/3 text-white px-20">
+      <div className="flex flex-wrap justify-center py-5 md:py-10 bg-gray-800 text-center md:text-left ">
+        <div className="w-auto md:w-1/3 text-white px-20 my-5 md:my-0">
           <h1 className="text-2xl font-semibold mb-3"> No. Telp</h1>
           <h1>{data.footer.noWa}</h1>
         </div>
-        <div className=" w-auto md:w-1/3 text-white px-10">
+        <div className=" w-auto md:w-1/3 text-white px-10 my-5 md:my-0">
           <h1 className="text-2xl font-semibold mb-3">Alamat</h1>
           <h1>{data.footer.alamat}</h1>
         </div>
-        <div className=" w-auto md:w-1/3 text-white px-10">
+        <div className=" w-auto md:w-1/3 text-white px-10 my-5 md:my-0">
           <h1 className="text-2xl font-semibold mb-3">Sosial Media</h1>
           <SosialMedia />
         </div>
