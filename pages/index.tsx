20c659eb266ac1894b5568799b28c6b0a6bc7166
iamhkmid <iamhkmid@gmail.com>
2021-03-03 20:53:14 +0700

    revisi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,38 @@
-import { GetStaticProps } from 'next'
-import Head from 'next/head'
-import { FC, useContext } from 'react'
-import Footer from '../src/components/layouts/Footer'
-import Navbar from '../src/components/layouts/Navbar'
-import Section1 from '../src/components/layouts/Section1'
-import Section2 from '../src/components/layouts/Section2'
-import Section3 from '../src/components/layouts/Section3'
-import Section4 from '../src/components/layouts/Section4'
-import { NavContext } from '../src/contexts/NavContextProvider'
-import data from '../data'
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { FC, useContext } from "react";
+import Footer from "../src/components/layouts/Footer";
+import Navbar from "../src/components/layouts/Navbar";
+import Section1 from "../src/components/layouts/Section1";
+import Section2 from "../src/components/layouts/Section2";
+import Section3 from "../src/components/layouts/Section3";
+import Section4 from "../src/components/layouts/Section4";
+import { NavContext } from "../src/contexts/NavContextProvider";
+import data from "../data";
 
-import { section1, spesifikasi, paketHarga, contents } from '../src/types/type'
+import { section1, spesifikasi, paketHarga, contents } from "../src/types/type";
 
 type Props = {
-  section1: section1
-  spesifikasi: spesifikasi
-  paketHarga: paketHarga
-  contents: contents
-}
+  section1: section1;
+  spesifikasi: spesifikasi;
+  paketHarga: paketHarga;
+  contents: contents;
+};
 
 export const getStaticProps: GetStaticProps = async (context) => {
-
   return {
     props: {
       section1: data.section1,
       spesifikasi: data.spesifikasi,
       paketHarga: data.paketHarga,
-      contents: data.contents
-    }
-  }
-}
-
+      contents: data.contents,
+    },
+    revalidate: 1,
+  };
+};
 
 const Home: FC<Props> = (props) => {
-  const { changeNavToggle } = useContext(NavContext)
+  const { changeNavToggle } = useContext(NavContext);
   return (
     <>
       <Head>
@@ -46,10 +45,10 @@ const Home: FC<Props> = (props) => {
         <Section2 spesifikasi={props.spesifikasi} />
         <Section3 paketHarga={props.paketHarga} />
         <Section4 contents={props.contents} />
-      </main >
+      </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
